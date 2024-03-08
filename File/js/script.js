fced6daffb08f95ceb890dc172311f9590b95a15
FahimReza <Fahimreza202@outlook.com>
2024-03-08 21:30:25 +0330

    Project completion

--- File/js/script.js
+++ File/js/script.js
@@ -1,3 +1,15 @@
+  const scriptURL = 'https://script.google.com/macros/s/AKfycbyrOczOoVMuHFLwcdPdFc0N9tGQhyVSWQGkBw00u9MxLO9rsGfD36nYifeWD26giZ0/exec'
+  const form = document.forms['submit-to-google-sheet']
+
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => console.log('Success!', response))
+      .catch(error => console.error('Error!', error.message))
+  })
+
+
+
 var TabLinks = document.getElementsByClassName('Tab-Links');
 var TabContents = document.getElementsByClassName('Tab-Contents');
 
@@ -11,3 +23,13 @@ function OpenTab(TabName){
     event.currentTarget.classList.add("Active-Link");
     document.getElementById(TabName).classList.add("Active-Tab");
 }
+
+
+var SideMenu = document.getElementById('SideMenu');
+
+function OpenMenu(){
+    SideMenu.style.right = '0';
+};
+function CloseMenu(){
+    SideMenu.style.right = '-200px';
+};
